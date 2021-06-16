8dbb206d991c21a90579a6acca4f33f07c890817
Pieter de Bruin <pdebruin@hotmail.com>
2021-06-16 11:26:19 +0200

    ps1

---
+++ delundo.ps1
@@ -0,0 +1,9 @@
+# login https://docs.microsoft.com/en-us/powershell/azure/get-started-azureps?view=azps-6.1.0
+Connect-AzAccount -UseDeviceAuthentication
+# list https://docs.microsoft.com/en-us/powershell/module/az.storage/?view=azps-6.0.0
+
+# delete https://github.com/Azure/azure-docs-powershell-samples/tree/master/storage
+# list deleted
+# del
+# list
+dir
